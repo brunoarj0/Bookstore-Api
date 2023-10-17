00400e390c82dbfea0c42327e8f17f95569f0891
Bruno Andrade <bruno.andrade@eng4tech.com.br>
2023-10-17 09:46:37 -0300

    feat: third commit

--- Books/Controllers/LoanController.cs
+++ Books/Controllers/LoanController.cs
@@ -1,6 +1,6 @@
 ﻿using Bookstore.Data;
-using Bookstore.Migrations;
 using Microsoft.AspNetCore.Mvc;
+using LoanModel = Bookstore.Models.LoanModel;
 
 namespace Bookstore.Controllers
 {
@@ -14,6 +14,58 @@ namespace Bookstore.Controllers
         public IActionResult Index()
         {
             var loan = _context.Loan;
+
+            return View(loan);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(LoanModel loan)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Loan.Add(loan);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Update(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var loan = _context.Loan.FirstOrDefault(l => l.Id == id);
+
+            if (loan == null)
+            {
+                return NotFound();
+            }
+
+            return View(loan);
+        }
+
+        [HttpPost]
+        public IActionResult Update(LoanModel loan)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Loan.Update(loan);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
 
             return View(loan);
         }
--- Books/Models/LoanModel.cs
+++ Books/Models/LoanModel.cs
@@ -1,10 +1,19 @@
-﻿namespace Bookstore.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookstore.Models;
 
 public class LoanModel
 {
     public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Digite o nome do Recebedor!")]
     public string Receiver { get; set; }
+
+    [Required(ErrorMessage = "Digite o nome do Fornecedor!")]
     public string Supplier { get; set; }
+
+    [Required(ErrorMessage = "Digite o nome do Livro!")]
     public string BookName { get; set; }
-    public DateTime LastModified { get; set; }
+
+    public DateTime LastModified { get; set; } = DateTime.Now;
 }
